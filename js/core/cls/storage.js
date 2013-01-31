3436903babf11b346374d4f9f0a3ee967f0d8647
Dream Hao <176206@qq.com>
2013-02-01 07:36:07 +0800

    v0.0.4
    增加了cls.storage对window的localStorage和seesionStorage进行了封�
    输出.save  .get .del .clear四个方法

---
+++ js/core/cls/storage.js
@@ -0,0 +1,36 @@
+﻿/**********************************************************
+数据存取(storage)模块
+**********************************************************/
+
+zyGame.cls.storage=function(){
+	if (arguments[0]=='local'){
+		this.ws=window.localStorage;
+	}else{
+		this.ws=window.sessionStorage;
+	}
+	this.length=this.ws.length;
+};
+
+
+//保存
+zyGame.cls.storage.prototype.save=function(key,value){
+	this.ws.setItem(key,value);
+	this.length=this.ws.length;
+};
+
+//获取
+zyGame.cls.storage.prototype.get=function(key){
+	return this.ws.getItem(key);
+};
+
+//移除
+zyGame.cls.storage.prototype.del=function(key){
+	this.ws.removeItem(key);
+	this.length=this.ws.length;
+};
+
+//清除
+zyGame.cls.storage.prototype.clear=function(){
+	this.ws.clear();
+	this.length=this.ws.length;
+};
